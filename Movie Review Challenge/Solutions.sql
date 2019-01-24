5492279143e849cdc4f5469010b2112945e2c24a
Seeam Islam <seeamislam25@gmail.com>
2019-01-24 15:27:01 -0500

    Complete Q6 and Q7

--- Movie Review Challenge/Solutions.sql
+++ Movie Review Challenge/Solutions.sql
@@ -7,37 +7,58 @@ where director = 'Steven Spielberg';
 /* Q2: Find all years that have a movie that received a rating of 4 or 5, and sort them in increasing order. */
 
 select distinct year
-from Movie join Rating 
-on Movie.mID = Rating.mID 
-/* First, we must make sure we are talking about the same movie, so we can use their IDs to verify (as they're common in both tables)*/
-where stars >= 4
-ORDER BY year ASC; /*Sort in ascending order */
+from Movie JOIN Rating using (mID)
+where stars = 4 OR stars = 5
+ORDER BY year ASC;
+
 
 /* Q3: Find the titles of all movies that have no ratings. */
 
-/* We need the titles and ratings
-As we can see, if the movie has no rating, then its mID does not exist inside the ratings table 
-Therefore, we must check for which titles does there not exist an mID inside the Ratings tabke*/
+/* If a movie has no rating, then it will not exist inside the Ratings table */
 
 select title
 from Movie
-where mID NOT IN (select mID from Rating);
+where mID not in (select mID from Rating);
 
 /* Q4: Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings with a NULL value for the date. */
 
-/* We need the names of the reviewers if they left the date = NULL 
-We must also ensure the person rating is the right person*/
+/* We must make sure the rating ID is the same, to avoid mixing up reviewers */
 
 select name
-from Reviewer JOIN Rating using (rID) /* Equate rID's to make sure its the same person */
-where ratingDate IS NULL; /* Check NULL */
+from Reviewer JOIN Rating using (rID) /* B/c rID is a common column in each */
+where ratingDate is NULL;
 
 
 /* Q5: Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. */
 
-select distinct name, title, stars, ratingDate
-from (Movie join Rating using (mID)) join Reviewer using (rID)
-ORDER BY NAME, TITLE, STARS;
+select name, title, stars, ratingDate
+from Movie, Reviewer, Rating
+where Movie.mID = Rating.mID AND Rating.rID = Reviewer.rID
+ORDER BY name, title, stars;
 
-/* Q6: For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer's name and the title of the movie. 
- */
+/* Q6: For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer's name and the title of the movie. */
+
+/* return name and title
+make sure the reviewer's ID lines up each time with the review 
+To see if he rated it again, you must compare it */
+
+/* SELECT name, title
+FROM movie, reviewer 
+WHERE rID = (
+  SELECT r1.rID 
+  FROM rating AS r1 JOIN rating AS r2 
+  ON r1.rID = r2.rID AND r1.mID = r2.mID 
+  WHERE r1.ratingDate > r2.ratingDate AND r1.stars > r2.stars) 
+
+AND mID = (SELECT r1.mID 
+FROM rating AS r1 JOIN rating AS r2 
+ON r1.rID = r2.rID AND r1.mID = r2.mID 
+WHERE r1.ratingDate > r2.ratingDate AND r1.stars > r2.stars
+); */
+
+/* Q7: For each movie that has at least one rating, find the highest number of stars that movie received. Return the movie title and number of stars. Sort by movie title. */
+
+select title, MAX(stars)
+from (select * from Movie JOIN Rating using (mID))
+GROUP BY mID
+ORDER BY title;
